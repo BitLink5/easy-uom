af32636c98e7afbc97c2419ba02dfe82b7185148
BitLink5 <BitLink5@outlook.com>
2024-02-07 23:51:47 +0800

    fix: 修改声明对应的代码

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,16 +1,10 @@
 import { defineStore } from 'pinia'
 
-export const useAuthStore = defineStore(
-  'auth',
-  () => {
-    const token = ref()
-    const lastSentSmsCodeTimestamp = ref()
-    return {
-      token,
-      lastSentSmsCodeTimestamp
-    }
-  },
-  {
-    persist: true
+export const useAuthStore = defineStore('auth', () => {
+  const token = ref()
+  const lastSentSmsCodeTimestamp = ref()
+  return {
+    token,
+    lastSentSmsCodeTimestamp
   }
-)
+})

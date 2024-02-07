9d88f1177f89c2a9834081eb9760a8e539a6b3c0
BitLink5 <BitLink5@outlook.com>
2024-02-08 00:22:58 +0800

    fix: 删除没删干净的 pinia 持久化插件

--- src/main.js
+++ src/main.js
@@ -3,14 +3,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router/index.js'
 import { createPinia } from 'pinia'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { initNaiveUIDiscreteApi } from '@/utils/init.js'
 
 async function init() {
   const app = createApp(App)
 
   const pinia = createPinia()
-  pinia.use(piniaPluginPersistedstate)
   app.use(pinia)
 
   initNaiveUIDiscreteApi()
